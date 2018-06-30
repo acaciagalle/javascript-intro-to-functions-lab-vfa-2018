8271b883938ed3c6a7e129082845b96ddf3fec19
Acacia Galle <acacia.galle@yahoo.com>
2018-06-30 20:44:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ function logShout(string){
 function logShout(string){
   console.log(whisper(string))}
 
-function sayHiToGrandma(string){
+function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
   return "I can't hear you"
 
 else if (string.toUpperCase() === string)
 return "YES INDEED"
 
-else if (string === "Ilove you, Grandma"
+else if (string === "Ilove you, Grandma")
 return "I love you, too"
+}
